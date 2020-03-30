c593cbffd8fa4bdda9b1c0913ca36a8d4c832874
hoganSean99 <hogan.sean1999@gmail.com>
2020-03-30 19:02:14 +0100

    tests updated

--- test.js
+++ test.js
@@ -12,39 +12,39 @@ describe('Server initialization', function() {
         server = app.listen(3000, done);
     });
 
-    describe('Check Pages exist', function() {
-        it('Check that Home page does exist', function (done) {
-            request('http://localhost:3000', function (error, response, body) {
-                expect(response.statusCode).to.equal(200);
-                done();
-            });
-        });
-        it('Check that Update Book page does exist', function (done) {
-            request('http://localhost:3000/updateBook', function (error, response, body) {
-                expect(response.statusCode).to.equal(200);
-                done();
-            });
-        });
-        it('Check that Add Book page does exist', function (done) {
-            request('http://localhost:3000/updateBook', function (error, response, body) {
-                expect(response.statusCode).to.equal(200);
-                done();
-            });
+describe('Check Pages exist', function() {
+    it('Check that Home page does exist', function (done) {
+        request('http://localhost:3000', function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
         });
     });
+    it('Check that Update Book page does exist', function (done) {
+        request('http://localhost:3000/updateBook', function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+    it('Check that Add Book page does exist', function (done) {
+        request('http://localhost:3000/updateBook', function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+       });
+    });
+});
 
-    describe('Check Pages do not exist', function() {
-        it('Check that About page does not exist', function (done) {
-            request('http://localhost:3000/about', function (error, response, body) {
-                expect(response.statusCode).to.equal(404);
-                done();
-            });
-        });
-        it('Check that Contact Us page does not exist', function (done) {
-            request('http://localhost:3000/contact', function (error, response, body) {
-                expect(response.statusCode).to.equal(404);
-                done();
-            });
+describe('Check Pages do not exist', function() {
+    it('Check that About page does not exist', function (done) {
+        request('http://localhost:3000/about', function (error, response, body) {
+            expect(response.statusCode).to.equal(404);
+            done();
         });
     });
+    it('Check that Contact Us page does not exist', function (done) {
+        request('http://localhost:3000/contact', function (error, response, body) {
+            expect(response.statusCode).to.equal(404);
+	    done();
+        });
+    });
+});
 });

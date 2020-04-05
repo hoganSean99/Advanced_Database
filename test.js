7ec020796c8eb2f3d4df342dffa5630e487716eb
hoganSean99 <hogan.sean1999@gmail.com>
2020-04-05 18:13:52 +0100

    tests created into function to avoid duplication

--- test.js
+++ test.js
@@ -8,43 +8,27 @@ describe('Server initialization', function() {
     var app = express();
     var server;
 
+    function testFunc(test_name, url, response_code){
+        it(test_name, function (done) {
+            request(url, function (error, response, body) {
+                expect(response.statusCode).to.equal(response_code);
+                done();
+            });
+        });
+    }
+
     before(function (done) {
         server = app.listen(3000, done);
     });
 
-describe('Check Pages exist', function() {
-    it('Check that Home page does exist', function (done) {
-        request('http://localhost:3000', function (error, response, body) {
-            expect(response.statusCode).to.equal(404);
-            done();
-        });
+    describe('Check Pages exist', function() {
+        testFunc('Check that Home page does exist', 'http://localhost:3000/', 404);
+        testFunc('Check that Update Book page does exist', 'http://localhost:3000/updateBook', 404);
+        testFunc('Check that Add Book page does exist', 'http://localhost:3000/addBook', 404);
     });
-    it('Check that Update Book page does exist', function (done) {
-        request('http://localhost:3000/updateBook', function (error, response, body) {
-            expect(response.statusCode).to.equal(404);
-            done();
-        });
-    });
-    it('Check that Add Book page does exist', function (done) {
-        request('http://localhost:3000/updateBook', function (error, response, body) {
-            expect(response.statusCode).to.equal(404);
-            done();
-       });
-    });
-});
 
-describe('Check Pages do not exist', function() {
-    it('Check that About page does not exist', function (done) {
-        request('http://localhost:3000/about', function (error, response, body) {
-            expect(response.statusCode).to.equal(404);
-            done();
-        });
+    describe('Check Pages do not exist', function() {
+        testFunc('Check that About page does not exist', 'http://localhost:3000/about', 404);
+        testFunc('Check that Contact Us page does not exist', 'http://localhost:3000/contact', 404);
     });
-    it('Check that Contact Us page does not exist', function (done) {
-        request('http://localhost:3000/contact', function (error, response, body) {
-            expect(response.statusCode).to.equal(404);
-	    done();
-        });
-    });
-});
 });

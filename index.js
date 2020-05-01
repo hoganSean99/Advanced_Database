44393284f6a1ea8b828a52e23021e89205f8c9c6
Sean Hogan <32335404+hoganSean99@users.noreply.github.com>
2020-05-01 21:56:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 
 
 
-const couchdb = new NodeCouchDB({host:'3.250.60.80',
+const couchdb = new NodeCouchDB({host:'3.250.64.6',
                                  protocol:'http',
                                  port:5984});
 
